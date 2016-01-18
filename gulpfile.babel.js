0d68abcce4d1c41ae4b7cd8afca2a26f03608b42
hirakiuc <hirakiuc@gmail.com>
2016-01-18 16:12:11 +0900

    use webpack.config.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,32 +2,14 @@
 
 var gulp = require('gulp')
 var webpack = require('webpack-stream')
+var webpackConfig = require('./webpack.config.js')
 
 gulp.task('build:js', () => {
   var src = './src/containers/Root.js'
   var dst = './dst'
 
-  var config = {
-    output: {
-      filename: '[name].js'
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel',
-          query: {
-            cacheDirectory: true,
-            presets: ['es2015', 'react']
-          }
-        }
-      ]
-    }
-  }
-
   return gulp.src(src)
-    .pipe(webpack(config))
+    .pipe(webpack(webpackConfig))
     .pipe(gulp.dest(dst))
 })
 
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  output: {
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
