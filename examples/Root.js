d8753a70ed3dad4fa3024135bcaeb88269e4da69
hirakiuc <hirakiuc@gmail.com>
2016-03-06 14:34:17 +0900

    remove empty line.

--- examples/Root.js
+++ examples/Root.js
@@ -17,7 +17,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         </TextWidget>
         <ClockWidget key="2" _grid={{x: 6, y: 0, w: 3, h: 2, static: true}}>1</ClockWidget>
         <TextWidget key="3" _grid={{x: 9, y: 0, w: 3, h: 4, static: true}} title={'6'} body={''}></TextWidget>
-
         <TextWidget key="4" _grid={{x: 0, y: 2, w: 3, h: 2, static: true}} title={'4'} body={''}></TextWidget>
         <TextWidget key="5" _grid={{x: 3, y: 2, w: 6, h: 2, static: true}} title={'5'} body={''}></TextWidget>
       </Board>
